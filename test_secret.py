8889f054df3af5eb52a6e6ec008368bd0bf87f70
Dhruti Avadhani <dhruti.avadhani2004@gmail.com>
2026-06-02 20:51:33 +0530

    testing new gitleaks test file

--- test_secret.py
+++ test_secret.py
@@ -1,2 +1,13 @@
-API_KEY = "sk_test_1234567890"
-PASSWORD = "admin12345"
+# test_secret.py - INTENTIONALLY VULNERABLE FILE FOR GITLEAKS TEST
+
+aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+password = "admin123"
+token = "abcd1234efgh5678ijkl9012mnop3456"
+
+private_key = """
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAu9FakeKeyData1234567890ABCDE
+-----END RSA PRIVATE KEY-----
+"""
